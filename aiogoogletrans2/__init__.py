b63f04666448e8037fd88992d65f3c730c4442bc
aleksei_vorobev_3 <vorobyoff@gmail.com>
2022-02-12 18:50:52 +0300

    First release

---
+++ aiogoogletrans2/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '0.0.1'
+
+
+from aiogoogletrans2.client import Translator
+from aiogoogletrans2.constants import LANGCODES, LANGUAGES  # noqa
